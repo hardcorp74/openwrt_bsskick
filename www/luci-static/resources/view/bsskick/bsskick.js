5164d814dcdee7caf1c2038fa263feafe35bc897
Konstantin Krasavin <hardcorp74@mail.ru>
2024-08-07 17:18:34 +0500

    Change logread parser script remove timer and mac filter luci web page change

--- www/luci-static/resources/view/bsskick/bsskick.js
+++ www/luci-static/resources/view/bsskick/bsskick.js
@@ -5,13 +5,12 @@
 
 return view.extend({
     render: function() {
-        var m, s;
+        var m, s, o;
         m = new form.Map('bsskick', _('Bsskick'),
             _('This is a service for checking Wi-Fi hosts that send message "bss_termination_delay" and reject change Wi-Fi AP.<p>Bsskick force disconnect Wi-Fi hosts from old Wi-Fi AP and it can bee used with Usteer and DAWN.<p>For new settings Apply need restart service bsskick in System - Startup or <code>/etc/init.d/bsskick restart</code>'));
         s = m.section(form.TypedSection, 'bsskick', _('Main option'));
         s.anonymous = true;
-        s.option(form.Value, 'time', _('Check interval'),
-            _('Value in sec'));
+        o = s.option(form.DynamicList, 'mac', _('List for ignore bsskick'), _('MAC address'));
         return m.render();
     },
 });
