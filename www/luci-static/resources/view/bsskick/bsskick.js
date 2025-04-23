474c18c83ae1dbfd8cb840662e76c047455992a2
Konstantin Krasavin <hardcorp74@mail.ru>
2025-04-23 15:19:36 +0500

    Fix bsskick change init script and make it working with ucitrack and using fifo for optimal process running

--- www/luci-static/resources/view/bsskick/bsskick.js
+++ www/luci-static/resources/view/bsskick/bsskick.js
@@ -6,11 +6,11 @@
 return view.extend({
     render: function() {
         var m, s, o;
-        m = new form.Map('bsskick', _('Bsskick'),
-            _('This is a service for checking Wi-Fi hosts that send message "bss_termination_delay" and reject change Wi-Fi AP.<p>Bsskick force disconnect Wi-Fi hosts from old Wi-Fi AP and it can bee used with Usteer and DAWN.<p>For new settings Apply need restart service bsskick in System - Startup or <code>/etc/init.d/bsskick restart</code>'));
-        s = m.section(form.TypedSection, 'bsskick', _('Main option'));
+        m = new form.Map('bsskick', _('Wireless Kick'),
+            _('This is a service for checking wireless clients that send message "bss_termination_delay" and reject change wireless AP.<p>Process bsskick force disconnect Wireless hosts from old Wireless AP.'));
+        s = m.section(form.TypedSection, 'bsskick', _('Settings'));
         s.anonymous = true;
-        o = s.option(form.DynamicList, 'mac', _('List for ignore bsskick'), _('MAC address'));
+        o = s.option(form.DynamicList, 'mac', _('List for ignore'), _('MAC address'));
         return m.render();
     },
 });
